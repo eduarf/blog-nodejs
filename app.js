a105549b383ac6132db853b8c721ccc1e3f4302b
eduarf <fraude_f@yahoo.com>
2023-01-07 17:01:15 +0300

    page_name active

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const pageRoute = require('./routers/pageRoute');
-const entryRoute = require('./routers/entryRoute');
+const pageRoute = require('./routes/pageRoute');
+const entryRoute = require('./routes/entryRoute');
 
 
 app.set('view engine', 'ejs');
 mongoose.set('strictQuery', true);
 mongoose.connect('mongodb://127.0.0.1:27017/own-blog').then(() => console.log('db connected successfully'));
 
+// MIDDLEWARES
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
